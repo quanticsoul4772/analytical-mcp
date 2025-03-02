353f80618853d99fe47ff779826495eebabeb8a9
quanticsoul4772 <rbsmith4@gmail.com>
2025-03-02 13:24:27 -0800

    Clean up unnecessary project files
    - Removed debug and temporary files from project root
    - Removed outdated tool configuration files
    - Streamlined project structure

--- simple-test.js
+++
@@ -1,4 +0,0 @@
-console.log('Environment Variables:');
-console.log('EXA_API_KEY:', process.env.EXA_API_KEY ? 'Set' : 'Not Set');
-console.log('ENABLE_RESEARCH_INTEGRATION:', process.env.ENABLE_RESEARCH_INTEGRATION);
-console.log('ENABLE_RESEARCH_CACHE:', process.env.ENABLE_RESEARCH_CACHE);
